8bfa6f2b4cf98720deea6d81ac539874d7b8f5be
unorsk <25188+unorsk@users.noreply.github.com>
2024-07-10 12:38:23 +0200

    cleaning up

--- cvss.ts
+++ cvss.ts
@@ -56,7 +56,7 @@ type CVSS40 = {
 
 type MetricType = "VC" | "VI" | "VA" | "SC" | "SI" | "SA";
 
-function parseCVSS40(cvss40: string): CVSS40 {
+export function parseCVSS40(cvss40: string): CVSS40 {
     cvss40 = cvss40.startsWith("CVSS:4.0") ? cvss40.slice("CVSS:4.0".length) : cvss40;
 
     return {
@@ -95,187 +95,51 @@ function parseCVSS40(cvss40: string): CVSS40 {
     }
 }
 
-const testAssertions = [
-    {cvss: "CVSS:4.0/AV:L/AC:L/AT:P/PR:L/UI:N/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N", score: 7.3},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:P/PR:N/UI:P/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N", score: 7.7},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:P/PR:N/UI:P/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N/E:U", score: 5.2},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:P/PR:N/UI:N/VC:H/VI:L/VA:L/SC:N/SI:N/SA:N", score: 8.3},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:P/PR:N/UI:N/VC:H/VI:L/VA:L/SC:N/SI:N/SA:N/CR:H/IR:L/AR:L/MAV:N/MAC:H/MVC:H/MVI:L/MVA:L", score: 8.1},
-    {cvss: "CVSS:4.0/AV:L/AC:L/AT:N/PR:N/UI:A/VC:L/VI:N/VA:N/SC:N/SI:N/SA:N", score: 4.6},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:A/VC:N/VI:N/VA:N/SC:L/SI:L/SA:N", score: 5.1},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:N/VI:N/VA:N/SC:L/SI:L/SA:N", score: 6.9},
-    {cvss: "CVSS:4.0/AV:L/AC:L/AT:N/PR:H/UI:N/VC:N/VI:N/VA:N/SC:H/SI:N/SA:N", score: 5.9},
-    {cvss: "CVSS:4.0/AV:L/AC:L/AT:N/PR:N/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H", score: 9.4},
-    {cvss: "CVSS:4.0/AV:P/AC:L/AT:N/PR:N/UI:N/VC:H/VI:H/VA:H/SC:N/SI:H/SA:N/S:P/V:D", score: 8.3},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:N/VA:N/SC:N/SI:N/SA:N/E:A", score: 8.7},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H/E:A", score: 10.0},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N/E:A", score: 9.3},
-    {cvss: "CVSS:4.0/AV:A/AC:L/AT:N/PR:N/UI:N/VC:N/VI:L/VA:N/SC:H/SI:N/SA:H", score: 6.4},
-    {cvss: "CVSS:4.0/AV:L/AC:L/AT:N/PR:H/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H/R:I", score: 9.3},
-    {cvss: "CVSS:4.0/AV:L/AC:L/AT:P/PR:H/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H/R:I", score: 8.7},
-    {cvss: "CVSS:4.0/AV:P/AC:L/AT:N/PR:N/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H", score: 8.6},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:L/UI:N/VC:H/VI:N/VA:N/SC:N/SI:N/SA:N", score: 7.1},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:P/PR:N/UI:N/VC:H/VI:N/VA:N/SC:N/SI:N/SA:N", score: 8.2},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:N/VI:N/VA:H/SC:N/SI:N/SA:L", score: 8.7},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:N/VI:N/VA:H/SC:N/SI:N/SA:L/E:U", score: 6.6},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:A/VC:N/VI:N/VA:N/SC:L/SI:L/SA:N", score: 5.1},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:L/UI:P/VC:N/VI:N/VA:N/SC:L/SI:L/SA:N", score: 5.1},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:P/PR:L/UI:N/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N", score: 7.7},
-    {cvss: "CVSS:4.0/AV:L/AC:L/AT:N/PR:H/UI:N/VC:H/VI:H/VA:N/SC:N/SI:N/SA:N", score: 8.3},
-    {cvss: "CVSS:4.0/AV:L/AC:L/AT:N/PR:H/UI:N/VC:H/VI:H/VA:N/SC:N/SI:N/SA:N/E:U", score: 5.6},
-    {cvss: "CVSS:4.0/AV:L/AC:L/AT:N/PR:N/UI:P/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N", score: 8.5},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:P/PR:N/UI:N/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N/E:A", score: 9.2},
-    {cvss: "CVSS:4.0/AV:P/AC:L/AT:P/PR:N/UI:N/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N", score: 5.4},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:N/VA:N/SC:N/SI:N/SA:N", score: 8.7},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:H/UI:N/VC:H/VI:N/VA:N/SC:N/SI:N/SA:N", score: 6.9},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N", score: 9.3},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:N/VI:N/VA:N/SC:N/SI:L/SA:N", score: 6.9},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:N/VI:L/VA:N/SC:L/SI:N/SA:H", score: 7.8},
-    {cvss: "CVSS:4.0/AV:L/AC:L/AT:N/PR:N/UI:P/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N/S:P", score: 8.5},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:L/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H/S:P/AU:Y/V:C/RE:L", score: 9.4},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:L/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H/E:P/CR:L/IR:H/AR:L/MAV:L/MAC:H/MAT:N/MPR:N/MUI:N/MVC:N/MVI:H/MVA:L/MSC:N/MSI:S/MSA:L", score: 7.0},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:L/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H/MAV:A/MAC:H/MAT:N/MPR:L/MUI:N/MVC:L/MVI:H/MVA:H/MSC:L/MSI:S/MSA:S/CR:L/IR:H/AR:H/E:P", score: 7.4},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:L/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H/E:P/CR:L/IR:H/AR:H/MAV:A/MAC:H/MAT:N/MPR:L/MUI:N/MVC:L/MVI:H/MVA:H/MSC:L/MSI:S/MSA:S", score: 7.4},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:L/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H/MAV:N/MAC:H/MAT:N/MPR:L/MUI:N/MVC:H/MVI:H/MVA:H/MSC:H/MSI:S/MSA:H/CR:M/IR:H/AR:M/E:P", score: 8.7},
-    {cvss: "CVSS:4.0/AV:A/AC:L/AT:N/PR:N/UI:N/VC:H/VI:H/VA:N/SC:N/SI:N/SA:N/S:P", score: 8.6},
-    {cvss: "CVSS:4.0/AV:A/AC:L/AT:N/PR:N/UI:N/VC:H/VI:H/VA:N/SC:N/SI:N/SA:N/MSI:S/S:P", score: 9.7},
-    {cvss: "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:N/VI:N/VA:H/SC:N/SI:N/SA:N/V:C", score: 8.7},
-]
+function calcMaxVectors([eq1, eq2, eq3, eq4, eq5, eq6]: number[]) {
 
+    const eq1_maxes = maxComposed[0][eq1]
+    const eq2_maxes = maxComposed[1][eq2]
+    const eq3_eq6_maxes = maxComposedEQ3[eq3][eq6] // macroVectorResult[5] -> eq6 because of JS :)
+    const eq4_maxes = maxComposed[3][eq4]
+    const eq5_maxes = maxComposed[4][eq5]
 
-testAssertions.forEach((e) => {
-    const cvss = parseCVSS40(e.cvss)
-    
-    const score = cvss40score(macroVector(cvss), cvss)
-    if (score != e.score) {
-        console.log(`${score}  expected: ${e.score} ${e.cvss}`)
+    const max_vectors = []
+    for (let eq1_max of eq1_maxes) {
+        for (let eq2_max of eq2_maxes) {
+            for (let eq3_eq6_max of eq3_eq6_maxes) {
+                for (let eq4_max of eq4_maxes) {
+                    for (let eq5max of eq5_maxes) {
+                        max_vectors.push(eq1_max + eq2_max + eq3_eq6_max + eq4_max + eq5max)
+                    }
+                }
+            }
+        }
     }
-})
+    return max_vectors
+  }
 
-// const cvss = parseCVSS40("CVSS:4.0/AV:L/AC:L/AT:P/PR:L/UI:N/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N")
-// console.log(cvss40score(macroVector(cvss), cvss))
-
-function cvss40score (macroVectorResult: number[], cvss: CVSS40) {
+export function cvss40score (macroVectorResult: number[], cvss: CVSS40) {
   if (["VC", "VI", "VA", "SC", "SI", "SA"].every((metric) => cvss[metric as MetricType] == "N")) {
       return 0.0
   }
 
-  let value = lookup[macroVectorResult.join("")]
-
-  // 1. For each of the EQs:
-  //   a. The maximal scoring difference is determined as the difference
-  //      between the current MacroVector and the lower MacroVector.
-  //     i. If there is no lower MacroVector the available distance is
-  //        set to NaN and then ignored in the further calculations.
   const [eq1, eq2, eq3, eq4, eq5, eq6] = macroVectorResult
 
-  // compute next lower macro, it can also not exist
-  const eq1_next_lower_macro = [eq1 + 1, eq2, eq3, eq4, eq5, eq6].join("")
-  const eq2_next_lower_macro = [eq1, eq2 + 1, eq3, eq4, eq5, eq6].join("")
+  const next_lower_macro = [
+    lookup[[eq1 + 1, eq2, eq3, eq4, eq5, eq6].join("")],
+    lookup[[eq1, eq2 + 1, eq3, eq4, eq5, eq6].join("")],
+    calc_score_eq3eq6_next_lower_macro(macroVectorResult),
+    lookup[[eq1, eq2, eq3, eq4 + 1, eq5, eq6].join("")],
+    lookup[[eq1, eq2, eq3, eq4, eq5 + 1, eq6].join("")],
+  ]
 
-  // eq3 and eq6 are related
-  //   const score_eq3eq6_next_lower_macro = calc_score_eq3eq6_next_lower_macro(eq3, eq6, eq1, eq2, eq4, eq5)
-  
-  const eq4_next_lower_macro = [eq1, eq2, eq3, eq4 + 1, eq5, eq6].join("")
-  const eq5_next_lower_macro = [eq1, eq2, eq3, eq4, eq5 + 1, eq6].join("")
+  const max_vectors = calcMaxVectors(macroVectorResult)
 
-  // get their score, if the next lower macro score do not exist the result is NaN
+  const current_severities = getSeverities(cvss, max_vectors)
 
-  //   b. The severity distance of the to-be scored vector from a
-  //      highest severity vector in the same MacroVector is determined.
-  const eq1_maxes = maxComposed[0][eq1]
-  const eq2_maxes = maxComposed[1][eq2]
-  const eq3_eq6_maxes = maxComposedEQ3[eq3][eq6] // macroVectorResult[5] -> eq6 because of JS :)
-  const eq4_maxes = maxComposed[3][eq4]
-  const eq5_maxes = maxComposed[4][eq5]
+  let value = lookup[macroVectorResult.join("")]
 
-  // compose them
-  const max_vectors = []
-  for (let eq1_max of eq1_maxes) {
-      for (let eq2_max of eq2_maxes) {
-          for (let eq3_eq6_max of eq3_eq6_maxes) {
-              for (let eq4_max of eq4_maxes) {
-                  for (let eq5max of eq5_maxes) {
-                      max_vectors.push(eq1_max + eq2_max + eq3_eq6_max + eq4_max + eq5max)
-                  }
-              }
-          }
-      }
-  }
-
-  const [
-    current_severity_distance_eq1,
-    current_severity_distance_eq2,
-    current_severity_distance_eq3eq6,
-    current_severity_distance_eq4,
-    current_severity_distance_eq5,
-  ] = getSeverities(cvss, max_vectors);
-
-  function calcMeanDistance() {
-    const step = 0.1
-
-    // some of them do not exist, we will find them by retrieving the score. If score null then do not exist
-    // todo get rid of the lets
-    let n_existing_lower = 0
-
-    let normalized_severity_eq1 = 0
-    let normalized_severity_eq2 = 0
-    let normalized_severity_eq3eq6 = 0
-    let normalized_severity_eq4 = 0
-    let normalized_severity_eq5 = 0
-
-
-    //   c. The proportion of the distance is determined by dividing
-    //      the severity distance of the to-be-scored vector by the depth
-    //      of the MacroVector.
-    //   d. The maximal scoring difference is multiplied by the proportion of
-    //      distance.
-    const available_distance_eq1 = value - lookup[eq1_next_lower_macro]
-    if (!isNaN(available_distance_eq1)) {
-        n_existing_lower = n_existing_lower + 1
-        const maxSeverity_eq1 = maxSeverityData[0][eq1] * step
-        const percent_to_next_eq1_severity = (current_severity_distance_eq1) / maxSeverity_eq1
-        normalized_severity_eq1 = available_distance_eq1 * percent_to_next_eq1_severity
-    }
-
-    const available_distance_eq2 = value - lookup[eq2_next_lower_macro]
-    if (!isNaN(available_distance_eq2)) {
-        n_existing_lower = n_existing_lower + 1
-        const maxSeverity_eq2 = maxSeverityData[1][eq2] * step
-        const percent_to_next_eq2_severity = (current_severity_distance_eq2) / maxSeverity_eq2
-        normalized_severity_eq2 = available_distance_eq2 * percent_to_next_eq2_severity
-    }
-
-    const available_distance_eq3eq6 = value - calc_score_eq3eq6_next_lower_macro(macroVectorResult)
-    if (!isNaN(available_distance_eq3eq6)) {
-        n_existing_lower = n_existing_lower + 1
-        const maxSeverity_eq3eq6 = maxSeverityeq3eq6[eq3][eq6] * step
-        const percent_to_next_eq3eq6_severity = (current_severity_distance_eq3eq6) / maxSeverity_eq3eq6
-        normalized_severity_eq3eq6 = available_distance_eq3eq6 * percent_to_next_eq3eq6_severity
-    }
-
-    const available_distance_eq4 = value - lookup[eq4_next_lower_macro]
-    if (!isNaN(available_distance_eq4)) {
-        n_existing_lower = n_existing_lower + 1
-        const maxSeverity_eq4 = maxSeverityData[3][eq4] * step
-        const percent_to_next_eq4_severity = (current_severity_distance_eq4) / maxSeverity_eq4
-        normalized_severity_eq4 = available_distance_eq4 * percent_to_next_eq4_severity
-    }
-
-    const available_distance_eq5 = value - lookup[eq5_next_lower_macro]
-    if (!isNaN(available_distance_eq5)) {
-        // for eq5 is always 0 the percentage
-        n_existing_lower = n_existing_lower + 1
-        const percent_to_next_eq5_severity = 0 // todo wtf
-        normalized_severity_eq5 = available_distance_eq5 * percent_to_next_eq5_severity
-    }
-
-    // 2. The mean of the above computed proportional distances is computed.
-    const mean_distance = n_existing_lower == 0 ? 0 : (normalized_severity_eq1 + normalized_severity_eq2 + normalized_severity_eq3eq6 + normalized_severity_eq4 + normalized_severity_eq5) / n_existing_lower
-    return mean_distance
-  }
-
-  const mean_distance = calcMeanDistance();
+  const mean_distance = calcMeanDistance(value, next_lower_macro, macroVectorResult, current_severities, eq6)
 
   // 3. The score of the vector is the score of the MacroVector
   //    (i.e. the score of the highest severity vector) minus the mean
@@ -289,6 +153,28 @@ function cvss40score (macroVectorResult: number[], cvss: CVSS40) {
   }
   return Math.round(value * 10) / 10
 }
+
+function calcMeanDistance(value: number, next_lower_macro: number[], macroVectorResult: number[], current_severities: number[], eq6: number) {
+    let n_existing_lower = 0
+
+    const normalized_severity = [ 0, 0, 0, 0, 0 ].map((norm_serv, i) => {
+        const available_distance_eqi = value - next_lower_macro[i]
+        if (!isNaN(available_distance_eqi)) {
+            n_existing_lower = n_existing_lower + 1
+            if (i == 4) {
+                return 0
+            }
+            const eqi = macroVectorResult[i]
+            const maxSeverity = (i != 2 ? maxSeverityData[i][eqi] : maxSeverityeq3eq6[eqi][eq6]) * 0.1// step is 0.1
+            const percent_to_next_eqi_severity = current_severities[i] / maxSeverity
+            return available_distance_eqi * percent_to_next_eqi_severity
+        }
+        return norm_serv
+    })
+
+    const mean_distance = n_existing_lower == 0 ? 0 : normalized_severity.reduce((s, c) => s+c, 0) / n_existing_lower
+    return mean_distance
+  }
 
 function getSeverities(cvss: CVSS40, max_vectors: string[] ) {
     // Find the max vector to use i.e. one in the combination of all the highests
@@ -339,18 +225,12 @@ function getSeverities(cvss: CVSS40, max_vectors: string[] ) {
         break
     }
 
-    const current_severity_distance_eq1 = severity_distance_AV + severity_distance_PR + severity_distance_UI
-    const current_severity_distance_eq2 = severity_distance_AC + severity_distance_AT
-    const current_severity_distance_eq3eq6 = severity_distance_VC + severity_distance_VI + severity_distance_VA + severity_distance_CR + severity_distance_IR + severity_distance_AR
-    const current_severity_distance_eq4 = severity_distance_SC + severity_distance_SI + severity_distance_SA
-    const current_severity_distance_eq5 = 0 // todo wtf this isn't used
-
     return [
-        current_severity_distance_eq1,
-        current_severity_distance_eq2,
-        current_severity_distance_eq3eq6,
-        current_severity_distance_eq4,
-        current_severity_distance_eq5,
+        severity_distance_AV + severity_distance_PR + severity_distance_UI, //EQ1
+        severity_distance_AC + severity_distance_AT, //EQ2
+        severity_distance_VC + severity_distance_VI + severity_distance_VA + severity_distance_CR + severity_distance_IR + severity_distance_AR, //EQ3EQ6
+        severity_distance_SC + severity_distance_SI + severity_distance_SA, // EQ4
+        0, // EQ5 TODO THIS IS ACTUALLY UNUSED
     ]
 }
 
@@ -413,7 +293,7 @@ function extractValueMetric(metric: string, cvssStr: string) {
 
 }
 
-function macroVector(cvss: CVSS40): number[] {
+export function macroVector(cvss: CVSS40): number[] {
   // EQ1: 0-AV:N and PR:N and UI:N
   //      1-(AV:N or PR:N or UI:N) and not (AV:N and PR:N and UI:N) and not AV:P
   //      2-AV:P or not(AV:N or PR:N or UI:N)
